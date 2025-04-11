3496099c8ff48496ebc536bc6bd53af456456774
Liam <liam@simplifybiz.com>
2025-04-11 11:30:53 -0600

    update vite directory

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,12 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        journalActivity: resolve(__dirname, "src/journal/index.html"),
+        activities: resolve(__dirname, "src/activities/index.html"),
+        breathing: resolve(__dirname, "src/breathing/index.html"),
+        listing: resolve(__dirname, "src/listing/index.html"),
+        profile: resolve(__dirname, "src/profile/index.html"),
+        reflection: resolve(__dirname, "src/reflection/index.html"),
       },
     },
   },
