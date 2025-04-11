c5a37befd1d86f37d299f6a6b4d635756b47329e
Liam <liam@simplifybiz.com>
2025-04-11 15:25:23 -0600

    Desktop CSS

--- src/js/navigation.mjs
+++ src/js/navigation.mjs
@@ -2,9 +2,29 @@
 export function navigationMenu() {
   const navigationMenuElement = document.querySelector("#navigationMenu");
 
-  navigationMenuElement.innerHTML = `
-  <a href="../index.html">Dashboard</a>
-  <a href="../activities/index.html">Activities</a>
-  <a href="../profile/index.html">Profile</a>
+  function updateMenu() {
+    if (innerWidth > 768) {
+      navigationMenuElement.innerHTML = `
+<a href="../index.html">Dashboard</a>
+    <a href="../journal/index.html">Journal</a>
+    <a href="../breathing/index.html">Breathe</a>
+    <a href="../listing/index.html">List</a>
+    <a href="../reflection/index.html">Reflect</a>
+    <a href="../profile/index.html">Profile</a>
   `;
+    } else {
+      navigationMenuElement.innerHTML = `
+    <a href="../index.html">Dashboard</a>
+    <a href="../activities/index.html">Activities</a>
+    <a href="../profile/index.html">Profile</a>
+    `;``
+    }
+  }
+
+  updateMenu()
+
+  window.addEventListener("resize", updateMenu);
+
+
 }
+
--- src/js/profile.js
+++ src/js/profile.js
@@ -1,11 +1,9 @@
-import { loadHeaderFooter, mobileMenuToggle } from "./utils.mjs"
-import { navigationMenu } from "./navigation.mjs"
+import { loadHeaderFooter, mobileMenuToggle } from "./utils.mjs";
+import { navigationMenu } from "./navigation.mjs";
 
 document.addEventListener("DOMContentLoaded", () => {
   loadHeaderFooter().then(() => {
     mobileMenuToggle();
     navigationMenu();
   });
-})
-
-
+});
