c5532227e615ead3146db8a8227ea7179b09eea5
Liam <liam@simplifybiz.com>
2025-04-12 16:21:08 -0600

    add vite login and sign up path

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,8 @@ export default defineConfig({
         listing: resolve(__dirname, "src/listing/index.html"),
         profile: resolve(__dirname, "src/profile/index.html"),
         reflection: resolve(__dirname, "src/reflection/index.html"),
+        login: resolve(__dirname, "src/login/index.html"),
+        signup: resolve(__dirname, "src/signup/index.html"),
       },
     },
   },
