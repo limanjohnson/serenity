4740652cd9d3332496b397b5c91b7cec240b64f4
Liam <liam@simplifybiz.com>
2025-04-12 16:29:12 -0600

    zen quote generator added to main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 import { loadHeaderFooter, mobileMenuToggle } from "./utils.mjs";
 import { navigationMenu } from "./navigation.mjs";
+import zenQuoteGeneration from "./zenquote.mjs";
 
 document.addEventListener("DOMContentLoaded", () => {
   loadHeaderFooter().then(() => {
     mobileMenuToggle();
     navigationMenu();
+    zenQuoteGeneration();
   });
 });
